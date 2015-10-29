819cfc34aa1bdff4d5bc5219b98dfc381bac76c3
MiloGuo <miloguoqi@gmail.com>
2015-10-29 09:26:06 -0700

    Update Test.java

--- src/com/QG/model/Test.java
+++ src/com/QG/model/Test.java
@@ -2,7 +2,7 @@ package com.sp.model;
 import java.util.*;
 public class Test {
 
-	//hashmap使用的简单案例
+	
 	
 	public static void main(String []args){
 		
@@ -15,19 +15,17 @@ public class Test {
 		
 		hm.remove("2");
 		hm.clear();
-		//取出所有key
 		
-		//使用迭代器
 		Iterator it=hm.keySet().iterator();
 		
 		while(it.hasNext()){
 			
-			//取出key
+			
 			String key=(String)it.next();
 			
 			
 			
-			//根据key取出value
+		
 			
 			String nums=(String)hm.get(key);
 			System.out.println("key===="+key+" num="+nums);
